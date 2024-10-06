14d8e3b921a6a76be0d0c35e349bb5d7e52f7097
Shubhankar Pandey <shubhankarpandey322@gmail.com>
2024-10-06 21:15:25 +0530

    Create Find the number of islands.java

---
+++ Find the number of islands.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tc = scanner.nextInt(); // Number of test cases
+        while (tc-- > 0) {
+            int n = scanner.nextInt(); // Number of rows
+            int m = scanner.nextInt(); // Number of columns
+            char[][] grid = new char[n][m];
+
+            // Read the grid input
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    grid[i][j] = scanner.next().charAt(0);
+                }
+            }
+            Solution obj = new Solution();
+            int ans = obj.numIslands(grid);
+            System.out.println(ans);
+        }
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int numIslands(char[][] grid) {
+            if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int numIslands = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return numIslands;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(grid, i - 1, j);    
+        dfs(grid, i + 1, j);    
+        dfs(grid, i, j - 1);    
+        dfs(grid, i, j + 1);    
+        dfs(grid, i - 1, j - 1); 
+        dfs(grid, i - 1, j + 1);
+        dfs(grid, i + 1, j - 1); 
+        dfs(grid, i + 1, j + 1); 
+    }
+}
